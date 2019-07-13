4c8fa5010eb03bcc81ff093fee2bdde6b58f6622
Robert J. Kusznier <rj-kusznier@inventati.org>
2019-07-12 21:33:10 -0500

    Fix unit tests

--- tests/unit/temperature-conversion.js
+++ tests/unit/temperature-conversion.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import Vue from 'vue';
 
 import { UNIT_CELCIUS, UNIT_FAHRENHEIT }
-  from '@/components/WeatherInfoUnitSelection'; 
+  from '@/components/WeatherInfoDisplayUnits'; 
 
 import weatherDataFormatting from '@/mixins/weatherDataFormatting';
 
